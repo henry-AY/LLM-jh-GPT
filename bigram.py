819d9a823d3f26f85a677b511f6cae865c94591a
Hy8012 <henryyost402@gmail.com>
2024-09-06 13:45:28 -0700

    train.py updated to bigram.py, updated code, added self-attention

--- bigram.py
+++ bigram.py
@@ -2,6 +2,16 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
+batch_size = 32 # how many independent sequences will we process in parallel?
+block_size = 8 # what is the maxmimum context length for predictions
+max_iters = 3000
+eval_interval = 300
+learning_rate = 1e-2
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+eval_iters = 200
+
+torch.manual_seed(1)
+
 
 with open('english_input_developed_by_AI.txt', 'r', encoding='utf-8') as f:
     text = f.read()
@@ -21,53 +31,34 @@ itos = {i : ch for i, ch in enumerate(chars) }
 encode = lambda s: [stoi[c] for c in s] # encoder: take a string, outut a list of integers
 decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
 
-#temp
-print(encode("hi there"))
-print(decode(encode("hi there")))
-
 data = torch.tensor(encode(text), dtype=torch.long)
-print(data.shape, data.dtype)
-print(data[0:1000])
-
 n = int(0.9*len(data)) # first 90% will train, rest validate
 train_data = data[:n]
 val_data = data[n:]
 
-block_size = 8
-train_data[:block_size + 1]
-
-# x = train_data[:block_size]
-# y = train_data[1:block_size + 1]
-# for t in range(block_size):
-#     context = x[:t + 1]
-#     target = y[t]
-#     print(f"when input is {context} the traget: {target}")
-
-torch.manual_seed(1)
-batch_size = 4
+train_data[:block_size + 1] #temp?
 
 def get_batch(split):
     data = train_data if split == 'train' else val_data
     ix = torch.randint(len(data) - block_size, (batch_size,))
     x = torch.stack([data[i : i + block_size] for i in ix])
     y = torch.stack([data[i + 1 : i + block_size + 1] for i in ix])
+    x, y = x.to(device), y.to(device)
     return x, y
 
-xb, yb = get_batch('train')
-print('inputs:')
-print(xb.shape)
-print(xb)
-print('targets:')
-print(yb.shape)
-print(yb)
-
-print('-----')
-
-for b in range(batch_size):
-    for t in range(block_size):
-        context = xb[b, : t + 1]
-        target = yb[b, t]
-        print(f"when input is {context.tolist()} the target: {target}")
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
 
 class BigramLanguageModel(nn.Module):
 
@@ -106,33 +97,41 @@ class BigramLanguageModel(nn.Module):
             idx = torch.cat((idx, idx_next), dim = 1) # (B, T + 1)
         return idx
 
-m = BigramLanguageModel(vocab_size)
-logits, loss = m(xb, yb)
-print(logits.shape)
-print(loss)
-print()
-print("Without training: ")
-print(decode(m.generate(torch.zeros((1, 1), dtype = torch.long), max_new_tokens = 200)[0].tolist()))
+model = BigramLanguageModel(vocab_size)
+m = model.to(device)
 
-optimizer = torch.optim.AdamW(m.parameters(), lr = 1e-3)
+# create a PyTorch optimizer
+optimizer = torch.optim.AdamW(model.parameters(), lr = learning_rate)
 
-batch_size = 32
-for steps in range(100000):
-
-    # sample a batch of data
+for iter in range(max_iters):
+    if iter % eval_interval == 0:
+        losses = estimate_loss()
+        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+    #sample a batch of data
     xb, yb = get_batch('train')
 
-    #eval the loss
-    logits, loss = m(xb, yb)
+    #evaluate the loss
+    logits, loss = model(xb, yb)
     optimizer.zero_grad(set_to_none = True)
     loss.backward()
     optimizer.step()
 
-print()
+#generate from the model
+context = torch.zeros((1, 1), dtype = torch.long, device = device)
+print(decode(m.generate(context, max_new_tokens = 500)[0].tolist()))
 
-temp_loss = loss.item()
-print("After training: ")
-print(decode(m.generate(torch.zeros((1, 1), dtype = torch.long), max_new_tokens = 200)[0].tolist()))
-print()
-print("Ending loss of", temp_loss)
-print()
+B, T, C = 4, 8, 2 # batch, time, channels
+x = torch.randn(B, T, C)
+
+xbow = torch.zeros((B, T, C))
+for b in range(B):
+    for t in range(T):
+        xprev = x[b, : t + 1] # (t, C)
+        xbow[b, t] = torch.mean(xprev, 0)
+
+tril = torch.tril(torch.ones(T, T))
+wei = torch.zeros((T, T))
+wei = wei.masked_fill(tril == 0, float('-inf'))
+wei = F.softmax(wei, dim = -1)
+xbow3 = wei @ x # (T, T) @ (B, T, C) --> (B, T, C)
+torch.allclose(xbow, xbow3)
