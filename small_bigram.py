f4b0baa4fb6fc977fe07a4a065a83fcf4c0148cd
Hy8012 <henryyost402@gmail.com>
2024-10-03 15:19:01 -0700

    eval_iter updated

--- small_bigram.py
+++ small_bigram.py
@@ -7,7 +7,7 @@ from torch.nn import functional as F
 batch_size = 32 # how many independent sequences will we process in parallel?
 block_size = 8 # what is the maxmimum context length for predictions
 max_iters = 3000
-eval_interval = 1
+eval_interval = 200
 learning_rate = 1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
