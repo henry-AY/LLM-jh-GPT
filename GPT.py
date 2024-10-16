fdd18b0936e07736fff5c0e5dd5713734fcae4c7
Hy8012 <henryyost402@gmail.com>
2024-10-15 22:54:25 -0700

    Hotfix (Epoch counter) updated, header license included

--- GPT.py
+++ GPT.py
@@ -1,3 +1,5 @@
+import random
+import sys
 import torch
 import torch.nn as nn
 from train import BigramLanguageModel
@@ -7,8 +9,9 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 with open('English/WarAndPeace.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
-""" SET THE SEED UNIVERSALLY TO '1' """
-torch.manual_seed(1)
+""" REPLACE WITH SINGLE SEED FOR REPEATABLE RESULTS"""
+random_seed = random.randint(0, sys.maxsize - 1)
+torch.manual_seed(random_seed)
 
 chars = sorted(list(set(text)))
 vocab_size = len(chars)
@@ -19,8 +22,13 @@ encode = lambda s: [stoi[c] for c in s] # encoder: take a string, outut a list o
 decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
 
 model = BigramLanguageModel().to(device)
-model.load_state_dict(torch.load("model/final_model_weights.pth"))
+model.load_state_dict(torch.load("model/final_model_weights.pth", weights_only=True))
 model.eval()
+
+path = "model/checkpoint.pth"
+checkpoint = torch.load(path, weights_only=True)
+
+curr_epoch = checkpoint['epoch']
 
 def generate_token(token, max_tokens):
     input_ids = torch.tensor([encode(token)], dtype=torch.long).to(device)
@@ -29,8 +37,12 @@ def generate_token(token, max_tokens):
 
 output = generate_token("War and Peace", max_tokens=500)
 
+#print GPT, and license
+print('jh-GPT: Mit license\nAuthors: henry-Ay, jgarc826\nhttps://github.com/henry-AY/LLM-jh-GPT')
+print(f'Random seed set to: {random_seed}\nEpoch: {curr_epoch}\n')
+
 f = open('output.txt', 'a')
-f.write(output + '\n\n')
+f.write(f'Output @ Epoch: {curr_epoch}\n{output}\n')
 f.close()
 
 #print to console
